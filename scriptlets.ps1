8990b368a76d64a42ac978b879016b3acb908e05
Roeland Cerfonteijn <NL12143@hotmail.com>
2025-09-24 14:39:33 +0200

    new file:   react-site-information/audit-report-post-fix.html 	new file:   react-site-information/audit-report-pre-fix.html 	modified:   react-site-information/package-lock.json 	modified:   scriptlets.ps1

--- scriptlets.ps1
+++ scriptlets.ps1
@@ -16,7 +16,16 @@ Save-PnPTenantTemplate -Template $kit -Out yourstarterkit.pnp
 Invoke-PnPTenantTemplate -Path starterkit.pnp
 
 CD ".\react-site-information\sharepoint\solution"
+
 npm install
+
+npm install -g audit-export
+npm audit --json | audit-export --path ./audit-report-pre-fix.html --title "SPFx Audit Report"
+
+npm audit fix
+npm audit fix --force
+npm audit --json | audit-export --path ./audit-report-post-fix.html --title "SPFx Audit Report (Post-Fix)"
+
 gulp serve
 
 gulp trust-dev-cert
